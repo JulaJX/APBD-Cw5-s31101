57935ee78d2c278c7dd2f1d98a0dde1cea8ed97e
JulaJX <jacekeo007@gmail.com>
2026-06-09 20:40:41 +0200

    fix swagger error + prepare program.cs

--- Program.cs
+++ Program.cs
@@ -1,20 +1,26 @@
-using Microsoft.EntityFrameworkCore;
 using APBD_DatabaseFirst.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<HospitalDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-
-app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
+app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
