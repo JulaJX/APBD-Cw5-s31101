63c2c66cedbd6dff13f8fd5cf78479c392661a6b
JulaJX <jacekeo007@gmail.com>
2026-06-09 21:16:05 +0200

    fix program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using APBD_DatabaseFirst.Services;
 using APBD_DatabaseFirst.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +10,8 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<HospitalDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IPatientService, PatientService>();
 
 var app = builder.Build();
 
